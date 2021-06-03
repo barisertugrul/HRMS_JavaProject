1f9d458acbff7e7f21eb3fa1e1d5a7fa613890ff
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-06-03 15:46:32 +0300

    Code Refactoring

--- src/main/java/kodlamaio/hrms/api/controllers/CvLanguagesController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CvLanguagesController.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +35,11 @@ public class CvLanguagesController {
 		this.languageService = languageService;
 	}
 
+
+	@PostMapping("/add")
+	public ResponseEntity<?> add(@Valid @RequestBody CvLanguage language) {
+		return ResponseEntity.ok(this.languageService.add(language));
+	}
 
 	@PostMapping("/getById")
 	public DataResult<CvLanguage> getById(int languageId){
--- src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
@@ -4,11 +4,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +21,6 @@ import kodlamaio.hrms.business.abstracts.CvLanguageService;
 import kodlamaio.hrms.business.abstracts.SkillService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
-import kodlamaio.hrms.entities.concretes.CvLanguage;
 import kodlamaio.hrms.entities.concretes.Skill;
 
 @RestController
@@ -27,9 +30,14 @@ public class SkillsController {
 
 	private SkillService skillService;
 
-	public SkillsController(SkillService languageService) {
+	public SkillsController(SkillService skillService) {
 		super();
-		this.skillService = languageService;
+		this.skillService = skillService;
+	}
+
+	@PostMapping("/add")
+	public ResponseEntity<?> add(@Valid @RequestBody Skill skill) {
+		return ResponseEntity.ok(this.skillService.add(skill));
 	}
 
 	@PostMapping("/getById")
