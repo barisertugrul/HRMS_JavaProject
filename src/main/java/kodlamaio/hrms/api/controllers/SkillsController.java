e5b1cf737b18b682804dcd7dc045afc834ddb2b5
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-06-03 15:49:10 +0300

    Update SkillsController.java

--- src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/SkillsController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import kodlamaio.hrms.business.abstracts.CvLanguageService;
 import kodlamaio.hrms.business.abstracts.SkillService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
