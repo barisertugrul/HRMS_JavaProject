24828edea748c138cb396ea60645d7b24949a404
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-06-03 06:06:44 +0300

    Update PhotoUploadHelper.java

--- src/main/java/kodlamaio/hrms/core/utilities/helpers/PhotoUploadHelper.java
+++ src/main/java/kodlamaio/hrms/core/utilities/helpers/PhotoUploadHelper.java
@@ -1,5 +1,6 @@
 package kodlamaio.hrms.core.utilities.helpers;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.web.multipart.MultipartFile;
@@ -8,4 +9,5 @@ import kodlamaio.hrms.core.utilities.results.DataResult;
 
 public interface PhotoUploadHelper {
 	DataResult<Map<String, String>> upload(MultipartFile file);
+	DataResult<Map> delete(String id) throws IOException;
 }
