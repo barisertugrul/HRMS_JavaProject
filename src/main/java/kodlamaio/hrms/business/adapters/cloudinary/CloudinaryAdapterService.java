7bf7bb0d6fd4c11a53071d074b2037d98261fcb3
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-06-04 12:26:04 +0300

    Update CloudinaryAdapterService.java

--- src/main/java/kodlamaio/hrms/business/adapters/cloudinary/CloudinaryAdapterService.java
+++ src/main/java/kodlamaio/hrms/business/adapters/cloudinary/CloudinaryAdapterService.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,41 +23,42 @@ public class CloudinaryAdapterService implements PhotoUploadHelper {
 
     private Cloudinary cloudinary;
     
-    private Map<String, String> valuesMap = new HashMap<>();
 
-    	public CloudinaryAdapterService() {
-    		valuesMap.put("cloud_name","Input your cloud name" );
-    		valuesMap.put("api_key", "Input your Cloudinary api key" );
-    		valuesMap.put("api_secret","Input your Cloudinary secret key" );
-    		cloudinary = new Cloudinary(valuesMap);
-    	}
+    @Autowired
+    public CloudinaryAdapterService() {
+    	Map<String, String> valuesMap = new HashMap<>();
+    	valuesMap.put("cloud_name","Input your cloud name" );
+    	valuesMap.put("api_key", "Input your Cloudinary api key" );
+    	valuesMap.put("api_secret","Input your Cloudinary secret key" );
+    	cloudinary = new Cloudinary(valuesMap);
+    }
     	
-    	public DataResult<Map<String, String>> upload(MultipartFile multipartFile) {
-    		File file;
-			try {
-				file = convert(multipartFile);
-	    		Map<String, String> result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
-	    		file.delete();
-	    		return new SuccessDataResult<Map<String, String>>(result);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return new ErrorDataResult<Map<String, String>>("Dosya yüklenemedi");
-			}
-    	}
+    public DataResult<Map<String, String>> upload(MultipartFile multipartFile) {
+    	File file;
+		try {
+			file = convert(multipartFile);
+	    	Map<String, String> result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+	    	file.delete();
+	    	return new SuccessDataResult<Map<String, String>>(result);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new ErrorDataResult<Map<String, String>>("Dosya yüklenemedi");
+		}
+    }
     	
-    	public DataResult<Map> delete (String id) throws IOException {
-    		Map result = cloudinary.uploader().destroy(id,ObjectUtils.emptyMap());
-    		return new SuccessDataResult<Map>(result);
-    	}
+    public DataResult<Map> delete (String id) throws IOException {
+    	Map result = cloudinary.uploader().destroy(id,ObjectUtils.emptyMap());
+    	return new SuccessDataResult<Map>(result);
+    }
     	
     	
-    	private File convert(MultipartFile multipartFile) throws IOException {
-    		File file = new File(multipartFile.getOriginalFilename());
-    		FileOutputStream stream = new FileOutputStream(file);
-    		stream.write(multipartFile.getBytes());
-    		stream.close();
+    private File convert(MultipartFile multipartFile) throws IOException {
+    	File file = new File(multipartFile.getOriginalFilename());
+    	FileOutputStream stream = new FileOutputStream(file);
+    	stream.write(multipartFile.getBytes());
+    	stream.close();
     		
-    		return file;
-    	}
+    	return file;
+    }
 
 }
